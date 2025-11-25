5f99b8341eab0a535df380326332c1edca21cf23
WeiLuo-1 <luo.1310@osu.edu>
2025-11-24 21:05:48 -0600

    add http://127.0.0.1 to backend app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,12 @@ function createApp() {
   const app = express()
 
   app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
